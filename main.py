5d26b6d73a149c830aa11f12b12b2c87512f4512
shimon3 <shmouel6@gmail.com>
2023-07-23 13:54:08 +0300

    This repository called two fonctions welcome and loag game

---
+++ main.py
@@ -0,0 +1,11 @@
+from Live import welcome,get_number
+from UpdatedMainGame import load_game
+
+#להציג את הWelcome
+welcome()
+
+# #לעלות את המשחק
+load_game()
+
+# #לבקש איזה משחק המשתמש רוצה לשחק
+# get_number()
