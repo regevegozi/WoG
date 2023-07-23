d7e8f35fd2f6d45ee922aef3b400e761b693d4ab
shimon3 <shmouel6@gmail.com>
2023-07-23 14:16:54 +0300

    i change the title of the choice of the game

--- UpdatedMainGame.py
+++ UpdatedMainGame.py
@@ -3,7 +3,7 @@ from GuessGame import play
 from CurrencyRouletteGame import play2
 
 def load_game():
-     game_choice = input("Choose game: 1 for Memory Game, 2 for Guess Game, 3 for Currency Roulette\n")
+     game_choice = input("Choose game: 1 for MemoryGame, 2 for GuessGame, 3 for CurrencyRoulette\n")
      if game_choice == "1":
          print("Memory Game - a sequence of numbers will appear for 1 second and you have o guess it back")
          play1()
@@ -11,7 +11,7 @@ def load_game():
          print ("Guess Game - guess a number and see if you choose like the computer")
          play()
      elif game_choice == "3":
-         print("Currency Roulette - essayez de deviner la valeur d'un montant aléatoire de USD en ILS")
+         print("Currency Roulette - try to guess the value of usd to ils")
          play2()
      else:
          print("Choix invalide")
